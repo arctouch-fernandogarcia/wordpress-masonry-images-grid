54ca708c169ede4edae5998b6e9cb8b09a992ad8
Fernando Garcia <fernandogarcia@Fernando-Garcias-MacBook-Pro-376A.local>
2022-03-25 11:46:19 -0300

    Created first build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '3392aa31ee97b01c51ed6acf8855eabb');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'cf5b93a721624ebae361a7b2c6dc0c13');
